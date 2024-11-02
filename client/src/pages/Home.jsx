91654d19f71d7f0d79fe4f9508c5f1435c8e795b
Shema Dabiri <dabirishema@gmail.com>
2024-11-02 11:09:58 -0600

    home fix

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,14 +2,16 @@
 function Home() {
   return (
     <>
-     <div className="jumbotron text-center">
-        <h1 className="display-4">Welcome to REC 2024!</h1>
-        <p className="lead">Employee Dashboard Homepage! Feel free to sign up your company to organize your company today!</p>
-        <hr className="my-4"/>
-        <p className="lead">
-          <a className="btn btn-primary btn-lg" href="#" role="button">Log In/Register</a>
-        </p>
-    </div>
+      <div style={{ height: '100vh' }} className="d-flex justify-content-center align-items-center">
+        <div className="jumbotron text-center">
+          <h1 className="display-4">Welcome to REC 2024!</h1>
+          <p className="lead">Employee Dashboard Homepage! Feel free to sign up your company to organize your company today!</p>
+          <hr className="my-4" />
+          <p className="lead">
+            <a className="btn btn-primary btn-lg" href="#" role="button">Log In/Register</a>
+          </p>
+        </div>
+      </div>
     </>
   )
 }
