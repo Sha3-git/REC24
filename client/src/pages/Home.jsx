c4269cd4e1cc1f60c00d60e018fd6d1e8150d883
Simran Brar <brarsimranbrar87@gmail.com>
2024-11-02 12:14:37 -0600

    form validation on signup

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,8 +2,8 @@
 function Home() {
   return (
     <>
-      <div style={{ height: '100vh' }} className="d-flex justify-content-center align-items-center">
-        <div className="jumbotron text-center">
+      <div style={{ height: '100vh'}} className="d-flex justify-content-center align-items-center">
+        <div className="jumbotron text-center"  style={{ width: '700px'}}>
           <h1 className="display-4">Welcome to REC 2024!</h1>
           <p className="lead">Employee Dashboard Homepage! Feel free to sign up your company to organize your company today!</p>
           <hr className="my-4" />
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,25 +1,73 @@
-
-
-
+import React, { useState } from 'react';
 import axios from 'axios';
+
 function Signup() {
-    //guid based redirection
+
+  const [inputPassword, setInputPassword] = useState('');
+  const [errorPassword, setErrorPassword] = useState('Password is required');
+
+  const [inputCheck, setInputCheck] = useState('');
+  const [errorCheck, setErrorCheck] = useState('Passwords do not match');
+
+  const handleInputChange1 = (e) => {
+    const value = e.target.value;
+    setInputPassword(value);
+    
+    if (!value.trim()) {
+      setErrorPassword('Password is required');
+    } else {
+      setErrorPassword('');
+    }
+  };
+
+  const handleInputChange2 = (e) => {
+    const value = e.target.value;
+    setInputCheck(value);
+    
+    if (value !== inputPassword) {
+      setErrorCheck('Passwords do not match');
+    } else {
+      setErrorCheck('');
+    }
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!errorPassword && !errorCheck) {
+    }
+  };
+
+
+
   return (
     <div 
     className="d-flex justify-content-center align-items-center vh-100"
   >
     <div className="p-5 bg-white shadow rounded" style={{ maxWidth: "500px"}}>
      
-    <form class="row g-3">
+    <form class="row g-3" onSubmit={handleSubmit}>
     <p className="lead text-center">Welcome to the onboarding, enter your password to complete the registration</p>
-    <div className="col-md-6" controlId="formBasicPassword">
-      <div class="form-label">Password</div>
-      <input class="form-control" type="password" placeholder="Password" />
-    </div>
-    <div className="col-md-6" controlId="formBasicPassword">
-      <div class="form-label">Re-Enter Password</div>
-      <input class="form-control" type="password" placeholder="Password" />
-    </div>
+    <div className="col-md-6">
+            <label className="form-label">Password</label>
+            <input 
+              className="form-control" 
+              type="password" 
+              placeholder="Password" 
+              value={inputPassword} 
+              onChange={handleInputChange1} 
+            />
+            {errorPassword && <div className="text-danger">{errorPassword}</div>}
+          </div>
+          <div className="col-md-6">
+            <label className="form-label">Re-Enter Password</label>
+            <input 
+              className="form-control" 
+              type="password" 
+              placeholder="Re-enter Password" 
+              value={inputCheck} 
+              onChange={handleInputChange2} 
+            />
+            {errorCheck && <div className="text-danger">{errorCheck}</div>}
+          </div>
 
     <button type="submit" class="btn btn-primary mb-3">Register for your company</button>
   </form>
