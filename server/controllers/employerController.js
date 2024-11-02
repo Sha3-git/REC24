36f236aaff5eac6bf142e73a78ba441fdb6fda4e
Maksim Sharoika <maximsharoika@gmail.com>
2024-11-02 10:36:29 -0600

    email go brr

--- server/controllers/employerController.js
+++ server/controllers/employerController.js
@@ -1,7 +1,19 @@
 const Employee = require('../models/employee');
 
 const createUser = async (req, res) => {
+    const transporter = nodemailer.createTransport({
+        host: 'kosichi.ca',
+        port: 465,
+        secure: true,
+        auth: {
+          user: process.env.USER_EMAIL,
+          pass: process.env.USER_PASS
+        },
+      });
+
     const {first_name, last_name, email, password, company_id} = req.body;
+    var userToken = null;
+
     try{
         const newEmployer = new Employee({
             first_name: first_name,
@@ -9,8 +21,38 @@ const createUser = async (req, res) => {
             email: email,
             company_id: company_id
         })  
-        newEmployer.save();
+
+        newEmployer.save().then(
+            savedEmployer => {
+                userToken = savedEmployer._id;
+            })
+            .catch(error => {
+                console.error(error);
+            });;
+
+        if (userToken != null) {
+            const userUrl = `https://prairiejobs.ca/reset-password/${userToken}`;
+        }
+
         res.Json({status: 200, data: newEmployer});
+  
+        const mailOptions = {
+            from: '"REC Kosichi" <rec@kosichi.ca>',
+            to: email,
+            subject: "REC Kosichi | New User Registration",
+            html: `
+              <p>Please complete the user registration on the following:</p>
+              <a href="${userUrl}">${userUrl}</a>
+            `,
+          };
+        
+          try {
+            const info = await transporter.sendMail(mailOptions);
+            console.log("Email sent:", info.response);
+          } catch (error) {
+            console.error("Error sending email:", error.message);
+          }
+
     }
     catch(error){
         res.status(500).json({ message: "Internal server error" });
