91df9483b324bcbf5129a4de6fc0c907e6832318
shema <dabirishema@gmail.com>
2024-11-02 00:38:07 -0600

    init libs

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({}));
+
+const port = 4000;
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
