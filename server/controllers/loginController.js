af78141db11f1f7109c1e6d1c1828cf5727d1b60
Maksim Sharoika <maximsharoika@gmail.com>
2024-11-02 13:45:09 -0600

    fixing

--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -4,16 +4,19 @@ const Employer = require('../models/employer');
 const login = async(req, res)=>{
     const {email, password} = req.body;
     console.log(typeof(email));
-    try{
-        const employee = Employee.findOne({email: email, password: password});
-        const employer  = Employer.findOne({email: email, password: password});
-        if (!employee[0] | !employer) {
+    try {
+        var employee = await Employee.findOne({ email: email, password: password });
+        var employer = await Employer.findOne({ email: email, password: password });
+    
+        if (!employee && !employer) {
             return res.json({ status: 404, message: "Login failed" });
         }
-        result = employee? employee : employer? employer: null;
-        res.json({status: 200, data: result})
-    }catch(error){
-        console.log(error)
+    
+        var result = employee ? employee : employer;
+    
+        res.json({ status: 200, data: result });
+    } catch (error) {
+        return res.json({ status: 500, message: "Internal server error" });
     }
 }
 
